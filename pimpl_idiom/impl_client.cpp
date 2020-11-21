e32de06b15aad94c3ce62dc08a3822b013f21f6f
KeyC0de <nik.lazkey@gmail.com>
2020-11-21 19:37:39 +0200

    Add project files.

---
+++ pimpl_idiom/impl_client.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <utility>
+#include "interface.h"
+
+// declare and define the implementation in the .cpp file
+struct Agent::Impl
+{
+    std::string m_name;
+	int m_id;
+	Impl( const std::string& name, int id )
+		:
+		m_name{name},
+		m_id{id}
+    {}
+    Impl( const Impl& rhs )
+	{
+		Impl temp{rhs};
+		std::swap( m_name, temp.m_name );
+		std::swap( m_id, temp.m_id );
+	}
+	Impl( Impl&& rhs ) noexcept
+		: m_name{std::move( rhs.m_name )},
+		m_id{std::move( rhs.m_id )}
+	{}
+	Impl& operator=( Impl&& rhs ) noexcept
+	{
+		if ( this != &rhs )
+		{
+			std::swap( m_name, rhs.m_name );
+			std::swap( m_id, rhs.m_id );
+		}
+		return *this;
+	}
+};
+
+std::ostream& operator<<( std::ostream& stream, Agent& rhs )
+{
+	return stream << rhs.getImplementation()->m_name
+		<< ':'
+		<< rhs.getImplementation()->m_id
+		<< '\n';
+}
+
+
+
+Agent::Agent( const std::string& name,
+	int id )
+	:
+	m_pimpl{ std::make_unique<Impl>( name, id ) }
+{}
+
+Agent::Agent( Agent&& rhs ) noexcept
+	:
+	m_pimpl{std::move( rhs.m_pimpl )}
+{}
+
+Agent& Agent::operator=( Agent&& rhs ) noexcept
+{
+	if ( this != &rhs )
+	{
+		std::swap( m_pimpl, rhs.m_pimpl );
+	}
+	return *this;
+}
+
+
+#include <iostream>
+
+int main()
+{
+	Agent e1{ "bradley", 700 };
+	Agent e2{ "john", 425 };
+	Agent e3{ "nikos", 1485 };
+
+	std::cout << e1;
+	e1.getImplementation()->m_name = "brad";
+	std::cout << e1;
+	std::cout << e2;
+	std::cout << e3;
+	e3.getImplementation()->m_name = "NameChanger";
+	e3.getImplementation()->m_id = 339;
+	std::cout << e3;
+
+	std::system( "pause" );
+	return 0;
+}
---
+++ pimpl_idiom/interface.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+// the interface class
+class Agent
+{
+	// This struct represents the implementation.
+	// That's all the compiler needs to know about this header file ie the size and layout of the pointer.
+	// The actual implementation may change of course but the pointer remains the same.
+	// As such we can achieve independent compilation.
+	// We only need a forward declaration of the pointer Type - which provides no size info
+	struct Impl;
+	std::unique_ptr<Impl> m_pimpl;
+public:
+	explicit Agent( const std::string& name, int id );
+	~Agent() = default;
+	Agent( const Agent& rhs ) = delete;
+	Agent& operator=( const Agent& rhs ) = delete;
+	Agent( Agent&& rhs ) noexcept;
+	Agent& operator=( Agent&& rhs ) noexcept;
+
+	std::unique_ptr<struct Impl>&& getImplementation() noexcept
+	{
+		if ( !m_pimpl )
+			return std::unique_ptr<Impl>{};
+		// returns the ownership of the resource exclusively - not the resource itself
+		return std::move( m_pimpl );
+	}
+
+	friend std::ostream& operator<<( std::ostream& stream, const Agent& rhs );
+};
